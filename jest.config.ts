048eeeb99641d26cab64de80b7c44bb523726635
Tavinder Singh <tavinder123singh@gmail.com>
2024-07-08 15:13:31 +0530

    initial commit

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import { Config } from "@jest/types";
+
+const config = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
+
+export default config;
---
+++ src/calculator.ts
@@ -0,0 +1,1 @@
+export default function calculate(numbers: string) {}
